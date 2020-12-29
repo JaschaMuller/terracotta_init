7c5138598dccffe6f2358a9490476ab8254232b9
Jascha Muller <jascha.muller@gmail.com>
2020-12-29 17:24:02 +0100

    wrong keys amount

--- terracotta_init.py
+++ terracotta_init.py
@@ -38,16 +38,16 @@ available_datasets = driver.get_datasets()
 
 #'http://landsat-pds.s3.amazonaws.com/
 #http://sentinel-cogs.s3.amazonaws.com/sentinel-s2-l2a-cogs/
-aws_rasters = [['http://sentinel-cogs.s3.amazonaws.com/sentinel-s2-l2a-cogs/34/H/BK/2019/2/S2B_34HBK_20190228_0_L2A/','S2','20190228'],
- ['http://sentinel-cogs.s3.amazonaws.com/sentinel-s2-l2a-cogs/33/H/YC/2019/2/S2B_33HYC_20190218_0_L2A/','S2','20190218'],
- ['http://sentinel-cogs.s3.amazonaws.com/sentinel-s2-l2a-cogs/34/H/BJ/2019/1/S2B_34HBJ_20190126_0_L2A/','S2','20190126'],
- ['http://sentinel-cogs.s3.amazonaws.com/sentinel-s2-l2a-cogs/34/H/BH/2019/2/S2B_34HBH_20190205_0_L2A/','S2','20190205'],
- ['http://sentinel-cogs.s3.amazonaws.com/sentinel-s2-l2a-cogs/34/H/CJ/2019/1/S2A_34HCJ_20190121_0_L2A/','S2','20190121'],
- ['http://sentinel-cogs.s3.amazonaws.com/sentinel-s2-l2a-cogs/33/H/YE/2019/2/S2A_33HYE_20190213_0_L2A/','S2','20190213'],
- ['http://sentinel-cogs.s3.amazonaws.com/sentinel-s2-l2a-cogs/34/H/CH/2019/1/S2B_34HCH_20190106_0_L2A/','S2','20190106'],
- ['http://sentinel-cogs.s3.amazonaws.com/sentinel-s2-l2a-cogs/34/H/CK/2019/1/S2A_34HCK_20190121_0_L2A/','S2','20190121'],
- ['http://sentinel-cogs.s3.amazonaws.com/sentinel-s2-l2a-cogs/34/H/DH/2019/1/S2B_34HDH_20190106_0_L2A/','S2','20190106'],
- ['http://sentinel-cogs.s3.amazonaws.com/sentinel-s2-l2a-cogs/33/H/YD/2019/1/S2A_33HYD_20190124_0_L2A/','S2','20190124']]
+aws_rasters = [['http://sentinel-cogs.s3.amazonaws.com/sentinel-s2-l2a-cogs/34/H/BK/2019/2/S2B_34HBK_20190228_0_L2A/','S2','34HBK','20190228'],
+ ['http://sentinel-cogs.s3.amazonaws.com/sentinel-s2-l2a-cogs/33/H/YC/2019/2/S2B_33HYC_20190218_0_L2A/','S2','33HYC','20190218'],
+ ['http://sentinel-cogs.s3.amazonaws.com/sentinel-s2-l2a-cogs/34/H/BJ/2019/1/S2B_34HBJ_20190126_0_L2A/','S2','34HBJ','20190126'],
+ ['http://sentinel-cogs.s3.amazonaws.com/sentinel-s2-l2a-cogs/34/H/BH/2019/2/S2B_34HBH_20190205_0_L2A/','S2','34HBH','20190205'],
+ ['http://sentinel-cogs.s3.amazonaws.com/sentinel-s2-l2a-cogs/34/H/CJ/2019/1/S2A_34HCJ_20190121_0_L2A/','S2','34HCJ','20190121'],
+ ['http://sentinel-cogs.s3.amazonaws.com/sentinel-s2-l2a-cogs/33/H/YE/2019/2/S2A_33HYE_20190213_0_L2A/','S2','33HYE','20190213'],
+ ['http://sentinel-cogs.s3.amazonaws.com/sentinel-s2-l2a-cogs/34/H/CH/2019/1/S2B_34HCH_20190106_0_L2A/','S2','34HCH','20190106'],
+ ['http://sentinel-cogs.s3.amazonaws.com/sentinel-s2-l2a-cogs/34/H/CK/2019/1/S2A_34HCK_20190121_0_L2A/','S2','34HCK','20190121'],
+ ['http://sentinel-cogs.s3.amazonaws.com/sentinel-s2-l2a-cogs/34/H/DH/2019/1/S2B_34HDH_20190106_0_L2A/','S2','34HDH','20190106'],
+ ['http://sentinel-cogs.s3.amazonaws.com/sentinel-s2-l2a-cogs/33/H/YD/2019/1/S2A_33HYD_20190124_0_L2A/','S2','33HYD','20190124']]
 
 #for raster_path in pbar:
 for raster_path in aws_rasters:
@@ -71,7 +71,7 @@ for raster_path in aws_rasters:
         with driver.connect():
             # since the rasters will be served from S3, we need to pass the correct remote path
             #driver.insert(keys, raster_path, override_path=f'{S3_PATH}/{raster_filename}')
-            driver.insert([raster_path[1], raster_path[2], band], raster_path[0]+band+'.tif')
+            driver.insert([raster_path[1], raster_path[2],raster_path[3], band], raster_path[0]+band+'.tif')
 print('created...')
 # upload database to S3
 #s3.meta.client.upload_file(DB_NAME, S3_BUCKET, DB_NAME)
